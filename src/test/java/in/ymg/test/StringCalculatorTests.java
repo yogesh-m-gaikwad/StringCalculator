079a07f3223524a4d85835f4cc9a7fb9ba95c897
Yogesh Gaikwad <yogeshmgaikwad@outlook.com>
2020-12-29 16:37:20 +0530

    Added tests for 0, 1, 2 numbers
    Tests : (* failing)
    an empty string returns 0
    *string with one number 7 returns the number as sum = 7
    *string with two numbers 5,4 returns sum = 9
    *string with two numbers 15,27 returns sum = 42

--- src/test/java/in/ymg/test/StringCalculatorTests.java
+++ src/test/java/in/ymg/test/StringCalculatorTests.java
@@ -13,10 +13,30 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * @author  Yogesh Gaikwad
  */
 public class StringCalculatorTests {
+
+    private final StringCalculator calculator = new StringCalculator();
+
     @Test
     @DisplayName("an empty string returns 0")
     void emptyStringTest() {
-        StringCalculator calculator = new StringCalculator();
         assertEquals(0, calculator.add(""), "empty string should return 0");
+    }
+
+    @Test
+    @DisplayName("string with one number 7 returns the number as sum = 7")
+    void oneIntegerStringToReturnTheNumberItSelfTest() {
+        assertEquals(7, calculator.add("7"), "String '7'  should return 7");
+    }
+
+    @Test
+    @DisplayName("string with two numbers 5,4 returns sum = 9")
+    void twoIntegerStringToReturnSumScenarioOneTest() {
+        assertEquals(9, calculator.add("5,4"), "String '5,4'  should return 9");
+    }
+
+    @Test
+    @DisplayName("string with two numbers 15,27 returns sum = 42")
+    void twoIntegerStringToReturnSumScenarioTwoTest() {
+        assertEquals(9, calculator.add("15,27"), "String '15,27'  should return 42");
     }
 }
